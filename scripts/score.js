ae5fb5e4d720c4deea14c60b236899dad907dc45
erikalouise <erikalouisesarmentovidal@gmail.com>
2024-08-07 11:37:38 -0300

    commitando

---
+++ scripts/score.js
@@ -0,0 +1,17 @@
+class Score {
+    constructor() {
+      this.value = 0;
+      this.maxSpeedInterval = 500; 
+      this.color = color(255); 
+    }
+
+    increment() {
+        this.value++;
+        this.updateAliensShootInterval();
+        this.updateScoreColor();
+      }
+    
+      get() {
+        return this.value;
+      }
+}
